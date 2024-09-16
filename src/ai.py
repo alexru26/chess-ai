94a57c6c7e53a45dd9f0a9ea36649f2667363c3c
alexru26 <rualex2008@gmail.com>
2024-09-16 15:00:56 -0400

    Update ai.py

--- src/ai.py
+++ src/ai.py
@@ -8,17 +8,12 @@ from keras import models
 
 class AI:
 
-    def __init__(self, engine='minimax', depth=3):
-        self.engine = engine
+    def __init__(self, depth=3):
         self.depth = depth
         self.model = models.load_model('../models/ai.keras')
         self.color = 'black'
         self.game_moves = []
         self.explored = 0
-
-    # -------
-    # MINIMAX
-    # -------
 
     def split_boards(self, board):
         """
@@ -67,131 +62,7 @@ class AI:
 
         return res
 
-    def heatmap(self, piece, row, col):
-        hmp = 0
-        if piece.name == 'pawn':
-            if piece.color == 'black':
-                hmp = [
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.02, 0.01, 0.00, 0.00, 0.00, 0.00, 0.01, 0.02],
-                    [0.01, 0.01, 0.03, 0.06, 0.06, 0.03, 0.01, 0.01],
-                    [0.02, 0.02, 0.04, 0.07, 0.07, 0.04, 0.02, 0.02],
-                    [0.03, 0.03, 0.05, 0.08, 0.08, 0.05, 0.03, 0.03],
-                    [0.07, 0.07, 0.08, 0.09, 0.09, 0.08, 0.07, 0.07],
-                    [0.10, 0.10, 0.10, 0.10, 0.10, 0.10, 0.10, 0.10],
-                    [9.00, 9.00, 9.00, 9.00, 9.00, 9.00, 9.00, 9.00],
-            ]
-            elif piece.color == 'white':
-                hmp = [
-                    [9.00, 9.00, 9.00, 9.00, 9.00, 9.00, 9.00, 9.00],
-                    [0.10, 0.10, 0.10, 0.10, 0.10, 0.10, 0.10, 0.10],
-                    [0.07, 0.07, 0.08, 0.09, 0.09, 0.08, 0.07, 0.07],
-                    [0.03, 0.03, 0.05, 0.08, 0.08, 0.05, 0.03, 0.03],
-                    [0.02, 0.02, 0.04, 0.07, 0.07, 0.04, 0.02, 0.02],
-                    [0.01, 0.01, 0.03, 0.06, 0.06, 0.03, 0.01, 0.01],
-                    [0.02, 0.01, 0.00, 0.00, 0.00, 0.00, 0.01, 0.02],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-            ]
-
-        elif piece.name == 'knight':
-            hmp = [
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.00],
-                    [0.00, 0.02, 0.06, 0.05, 0.05, 0.06, 0.02, 0.00],
-                    [0.00, 0.03, 0.05, 0.10, 0.10, 0.05, 0.03, 0.00],
-                    [0.00, 0.03, 0.05, 0.10, 0.10, 0.05, 0.03, 0.00],
-                    [0.00, 0.02, 0.06, 0.05, 0.05, 0.06, 0.02, 0.00],
-                    [0.00, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-            ]
-
-        elif piece.name == 'bishop':
-            hmp = [
-                    [0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02],
-                    [0.01, 0.05, 0.03, 0.03, 0.03, 0.03, 0.05, 0.01],
-                    [0.01, 0.03, 0.07, 0.05, 0.05, 0.07, 0.03, 0.01],
-                    [0.01, 0.03, 0.05, 0.10, 0.10, 0.05, 0.03, 0.01],
-                    [0.01, 0.03, 0.05, 0.10, 0.10, 0.05, 0.03, 0.01],
-                    [0.01, 0.03, 0.07, 0.05, 0.05, 0.07, 0.03, 0.01],
-                    [0.01, 0.05, 0.03, 0.03, 0.03, 0.03, 0.05, 0.01],
-                    [0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02],
-            ]
-
-        elif piece.name == 'king':
-            if piece.color == 'black':
-                hmp = [
-                    [0.05, 0.50, 0.10, 0.00, 0.00, 0.00, 0.10, 0.05],
-                    [0.02, 0.02, 0.00, 0.00, 0.00, 0.00, 0.02, 0.02],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                ]
-
-            elif piece.color == 'white':
-                hmp = [
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.02, 0.02, 0.00, 0.00, 0.00, 0.00, 0.02, 0.02],
-                    [0.05, 0.50, 0.10, 0.00, 0.00, 0.00, 0.10, 0.05],
-                ]
-
-        else :
-            hmp = [
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                    [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-            ]
-
-        eval = -hmp[row][col] if piece.color == 'black' else hmp[row][col]
-        return eval
-
-    def threats(self, board, piece):
-        eval = 0
-        for move in piece.moves:
-            attacked = board.squares[move.final.row][move.final.col]
-            if attacked.has_piece():
-                if attacked.piece.color != piece.color:
-                    # checks
-                    if attacked.piece.name == 'king':
-                        eval += attacked.piece.value / 10500
-
-                    # threat
-                    else:
-                        eval += attacked.piece.value / 45
-
-        return eval
-
     def static_eval(self, board):
-        # var
-        # eval = 0
-
-        # for row in range(ROWS):
-        #     for col in range(COLS):
-        #         if board.squares[row][col].has_piece():
-        #             # piece
-        #             piece =  board.squares[row][col].piece
-        #             # white - black
-        #             eval += piece.value
-        #             # heatmap
-        #             eval += self.heatmap(piece, row, col)
-        #             # moves
-        #             if piece.name != 'queen': eval += 0.01 * len(piece.moves)
-        #             else: eval += 0.003 * len(piece.moves)
-        #             # checks
-        #             eval += self.threats(board, piece)
-
         data = np.array([self.split_boards(board)])
 
         eval = self.model.predict(data, verbose=0)[0][0]
@@ -261,10 +132,6 @@ class AI:
 
             return min_eval, best_move # eval, move
 
-    # ---------
-    # MAIN EVAL
-    # ---------
-
     def eval(self, main_board):
         self.explored = 0
 
@@ -272,20 +139,16 @@ class AI:
         last_move = main_board.last_move
         self.game_moves.append(last_move)
 
-        # minimax engine
-        if self.engine == 'minimax':
-            # printing
-            print('\nFinding best move...')
-                        
-            # minimax initial call
-            eval, move = self.minimax(main_board, self.depth, False, -math.inf, math.inf) # eval, move
-            
-            # printing
-            print('\n- Initial eval:',self.static_eval(main_board))
-            print('- Final eval:', eval)
-            print('- Boards explored', self.explored)
-            if eval >= 5000: print('* White MATE!')
-            if eval <= -5000: print('* Black MATE!')
+        # printing
+        print('\nFinding best move...')
+
+        # minimax initial call
+        eval, move = self.minimax(main_board, self.depth, False, -math.inf, math.inf) # eval, move
+
+        # printing
+        print('\n- Initial eval:',self.static_eval(main_board))
+        print('- Final eval:', eval)
+        print('- Boards explored', self.explored)
             
         # append
         self.game_moves.append(move)
