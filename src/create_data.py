93ed6bd2abcad4c619c088eb2b76aa21008aa1f0
alexru26 <rualex2008@gmail.com>
2024-09-16 07:33:56 -0400

    Update everything

--- src/create_data.py
+++ src/create_data.py
@@ -1,32 +1,21 @@
 import csv
-import random
 
 data = []
 i = 0
-
-fid = open("../data/chessData.csv", "r")
-li = fid.readlines()[1:]
-fid.close()
-random.shuffle(li)
-fid = open("../data/shuffledAll.csv", "w")
-fid.writelines(li)
-fid.close()
-
-print('shuffled')
 
 def writeToFile():
     with open('../data/data'+str(i)+'.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['FEN', 'Evaluation'])
-        random.shuffle(data)
         writer.writerows(data)
 
-with open('../data/shuffledAll.csv', mode ='r') as file:
+with open('../data/chessData.csv', mode ='r') as file:
     counter = 0
     csvFile = csv.reader(file)
+    next(csvFile)
     for row in csvFile:
         if i == 10: break
-        if counter == 500000:
+        if counter == 100000:
             writeToFile()
             i += 1
             counter = 0
